1a212c946dc64224a7b15d06a0c2c89cc1181c1b
toby-zheng <tobyjzheng@gmail.com>
2025-04-17 01:33:39 -0500

    dac

--- HW5/dac/dac.c
+++ HW5/dac/dac.c
@@ -4,116 +4,132 @@
 #include "hardware/spi.h"
 
 // SPI Defines
-// We are going to use SPI 0, and allocate it to the following GPIO pins
-// Pins can be changed, see the GPIO function select table in the datasheet for information on GPIO assignments
 #define SPI_PORT spi0
 #define PIN_MISO 16
-#define PIN_CS   20     // can be any GPIO
+#define DAC_CS   20
+#define RAM_CS   14
 #define PIN_SCK  18
 #define PIN_MOSI 19
 
-float sine_array[200];
-float triangle_array[100]; 
+union FloatInt {
+    float f;
+    uint32_t i;
+};
 
 static inline void cs_select(uint cs_pin) {
-    asm volatile("nop \n nop \n nop"); // FIXME
+    asm volatile("nop \n nop \n nop");
     gpio_put(cs_pin, 0);
-    asm volatile("nop \n nop \n nop"); // FIXME
+    asm volatile("nop \n nop \n nop");
 }
 
 static inline void cs_deselect(uint cs_pin) {
-    asm volatile("nop \n nop \n nop"); // FIXME
+    asm volatile("nop \n nop \n nop");
     gpio_put(cs_pin, 1);
-    asm volatile("nop \n nop \n nop"); // FIXME
+    asm volatile("nop \n nop \n nop");
 }
 
-void wave_array_init();
 void writeDAC(char dac_select, float voltage);
+void spi_ram_init();
+void spi_write_float(uint16_t addr, float value);
+float spi_read_float(uint16_t addr);
 
-int main()
-{
+int main() {
     stdio_init_all();
 
-    // SPI initialisation. This example will use SPI at 1MHz.
-    spi_init(SPI_PORT, 1000000);
+    spi_init(SPI_PORT, 10000000);
     gpio_set_function(PIN_MISO, GPIO_FUNC_SPI);
-    gpio_set_function(PIN_CS,   GPIO_FUNC_SIO);
-    gpio_set_function(PIN_SCK,  GPIO_FUNC_SPI);
+    gpio_set_function(DAC_CS, GPIO_FUNC_SIO);
+    gpio_set_function(RAM_CS, GPIO_FUNC_SIO);
+    gpio_set_function(PIN_SCK, GPIO_FUNC_SPI);
     gpio_set_function(PIN_MOSI, GPIO_FUNC_SPI);
-    
-    // Chip select is active-low, so we'll initialise it to a driven-high state
-    gpio_set_dir(PIN_CS, GPIO_OUT);
-    gpio_put(PIN_CS, 1);
-    // For more examples of SPI use see https://github.com/raspberrypi/pico-examples/tree/master/spi
 
-    wave_array_init();
-    
+    gpio_init(DAC_CS);
+    gpio_init(RAM_CS);
+    gpio_set_dir(DAC_CS, GPIO_OUT);
+    gpio_put(DAC_CS, 1);
+    gpio_set_dir(RAM_CS, GPIO_OUT);
+    gpio_put(RAM_CS, 1);
+
+    spi_ram_init();
+
+    for (int i = 0; i < 1000; i++) {
+        float t = (float)i / 1000.0f;
+        float val = 1.65f + 1.65f * sinf(2 * M_PI * t);
+        spi_write_float(i * 4, val);
+    }
+
     int i = 0;
-    int j = 0;
-        while (true) {
-            // 2 sine cycles per second, 1 triangleke
-            printf("Hello, world!\n");
-            writeDAC('A', sine_array[i]);
-            writeDAC('B', triangle_array[j]);
-            i++;
-            j++;
-            sleep_us(2500);
-            writeDAC('A', sine_array[i]);
-            i++;
-            sleep_us(2500);
-            writeDAC('A', sine_array[i]);
-            i++;
-            sleep_us(2500);
-            writeDAC('A', sine_array[i]);
-            i++;
-            sleep_us(2500);
-            if (i >= 200) i = 0;
-            if (j >= 100) j = 0;
-        }
+    while (true) {
+        float val = spi_read_float(i * 4);
+        writeDAC('A', val);
+        sleep_ms(1);
+        i = (i + 1) % 1000;
     }
- 
-void wave_array_init() {
-    for (int i = 0; i < 200; i++) {
-        float t = (float) i / 200.0;
-        sine_array[i] = 1.65 + 1.65*sinf(2 * 3.14159 * 1.0 * t);
-    }
+}
 
-    for (int i = 0; i < 50; i++) {
-        float value = 3.3 * (float) i / 49.0;
-        triangle_array[i] = value;
-        triangle_array[i + 50] = 3.3 - value;
-    }
+void spi_ram_init() {
+    cs_select(RAM_CS);
+    uint8_t init[2] = {0x01, 0x40};  // Enable sequential mode
+    spi_write_blocking(SPI_PORT, init, 2);
+    cs_deselect(RAM_CS);
+}
+
+void spi_write_float(uint16_t addr, float val) {
+    union FloatInt num;
+    num.f = val;
+
+    uint8_t cmd[7];
+    cmd[0] = 0x02;                   
+    cmd[1] = (addr >> 8) & 0xFF;
+    cmd[2] = addr & 0xFF;
+    cmd[3] = num.i & 0xFF;
+    cmd[4] = (num.i >> 8) & 0xFF;
+    cmd[5] = (num.i >> 16) & 0xFF;
+    cmd[6] = (num.i >> 24) & 0xFF;
+
+    cs_select(RAM_CS);
+    spi_write_blocking(SPI_PORT, cmd, 7);
+    cs_deselect(RAM_CS);
+}
+
+float spi_read_float(uint16_t addr) {
+    uint8_t cmd[3];
+    uint8_t read[4];
+
+    cmd[0] = 0x03;                    
+    cmd[1] = (addr >> 8) & 0xFF;
+    cmd[2] = addr & 0xFF;
+
+    cs_select(RAM_CS);
+    spi_write_blocking(SPI_PORT, cmd, 3);
+    spi_read_blocking(SPI_PORT, 0x00, read, 4);
+    cs_deselect(RAM_CS);
+
+    union FloatInt num;
+    num.i = read[0] | (read[1] << 8) | (read[2] << 16) | (read[3] << 24);
+    return num.f;
 }
 
 void writeDAC(char dac_select, float voltage) {
     uint16_t msg, voltage_bits;
     uint8_t data[2];
     char dac_select_bit;
-    
-    // DAC output select
+
     switch (dac_select) {
-        case 'A':
-            dac_select_bit = 0;
-            break;
-        case 'B':
-            dac_select_bit = 1;
-            break;
-        default:
-            printf("!!!Error in DAC Select bit\n");
-            break;
+        case 'A': dac_select_bit = 0; break;
+        case 'B': dac_select_bit = 1; break;
+        default: printf("!!!Error in DAC Select bit\n"); return;
     }
-    
-    // voltage conversion V to bits
-    voltage_bits = (int) ((voltage/3.3) * 1024.0); 
-    // clamping
+
+    voltage_bits = (int)((voltage / 3.3f) * 1024.0f);
     if (voltage_bits > 1023) voltage_bits = 1023;
     if (voltage_bits < 0) voltage_bits = 0;
 
-    // bit manipulation --> data transfer
     msg = (dac_select_bit << 15) | (0b111 << 12) | (voltage_bits << 2);
-    data[0] = (msg) >> 8;     // MS Byte
-    data[1] = ((msg) & 0xFF);               
-    cs_select(PIN_CS);
+    data[0] = msg >> 8;
+    data[1] = msg & 0xFF;
+
+    cs_select(DAC_CS);
     spi_write_blocking(SPI_PORT, data, 2);
-    cs_deselect(PIN_CS);
+    cs_deselect(DAC_CS);
 }

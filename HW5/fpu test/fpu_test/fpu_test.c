7c480e757e17ec3dea83f2ec0bd1e745d8a1f90b
toby-zheng <tobyjzheng@gmail.com>
2025-04-17 01:24:19 -0500

    fpu corrected

--- HW5/fpu test/fpu_test/fpu_test.c
+++ HW5/fpu test/fpu_test/fpu_test.c
@@ -1,55 +1,63 @@
 #include <stdio.h>
 #include "pico/stdlib.h"
 
+#define CYCLE_NS 6.667
 
-int main()
-
-{
+int main() {
     stdio_init_all();
+    while (!stdio_usb_connected()) { ; }
+    sleep_ms(10);
 
     volatile float f1, f2;
-    absolute_time_t start, end;
     uint64_t elapsed;
+    float cycles;
 
-    printf("Enter two floats to use:");
+    printf("Enter two floats to use:\n");
     scanf("%f %f", &f1, &f2);
+
     volatile float f_add, f_sub, f_mult, f_div;
 
-    start = get_absolute_time();
+    absolute_time_t start = get_absolute_time();
     for (int i = 0; i < 1000; i++) {
-        f_add = f1+f2;
+        f_add = f1 + f2;
     }
-    end = get_absolute_time();
-    elapsed = to_us_since_boot(end) - to_us_since_boot(start);
-    printf("Addition avg time = %.3f ns\n", (elapsed * 1000.0) / 1000);
+    absolute_time_t end = get_absolute_time();
+    elapsed = to_us_since_boot(end - start);
+    cycles = ((float)elapsed * 1000.0) / CYCLE_NS;
+    printf("Time for 1000 additions: %llu us\n", elapsed);
+    printf("Estimated cycles per addition: %.2f\n\n", cycles / 1000.0);
 
-    start = get_absolute_time();
+    absolute_time_t start1 = get_absolute_time();
     for (int i = 0; i < 1000; i++) {
-        f_add = f1-f2;
+        f_sub = f1 - f2;
     }
-    end = get_absolute_time();
-    elapsed = to_us_since_boot(end) - to_us_since_boot(start);
-    printf("Subtraction avg time = %.3f ns\n", (elapsed * 1000.0) / 1000);
+    absolute_time_t end1 = get_absolute_time();
+    elapsed = to_us_since_boot(end1 - start1);
+    cycles = ((float)elapsed * 1000.0) / CYCLE_NS;
+    printf("Time for 1000 subtractions: %llu us\n", elapsed);
+    printf("Estimated cycles per subtraction: %.2f\n\n", cycles / 1000.0);
 
-    start = get_absolute_time();
+    absolute_time_t start2 = get_absolute_time();
     for (int i = 0; i < 1000; i++) {
-        f_add = f1*f2;
+        f_mult = f1 * f2;
     }
-    end = get_absolute_time();
-    elapsed = to_us_since_boot(end) - to_us_since_boot(start);
-    printf("Multiplication avg time = %.3f ns\n", (elapsed * 1000.0) / 1000);
-    
-    start = get_absolute_time();
+    absolute_time_t end2 = get_absolute_time();
+    elapsed = to_us_since_boot(end2 - start2);
+    cycles = ((float)elapsed * 1000.0) / CYCLE_NS;
+    printf("Time for 1000 multiplications: %llu us\n", elapsed);
+    printf("Estimated cycles per multiplication: %.2f\n\n", cycles / 1000.0);
+
+    absolute_time_t start3 = get_absolute_time();
     for (int i = 0; i < 1000; i++) {
-        f_add = f1/f2;
+        f_div = f1 / f2;
     }
-    end = get_absolute_time();
-    elapsed = to_us_since_boot(end) - to_us_since_boot(start);
-    printf("Division avg time = %.3f ns\n", (elapsed * 1000.0) / 1000);
-    
-    
+    absolute_time_t end3 = get_absolute_time();
+    elapsed = to_us_since_boot(end3 - start3);
+    cycles = ((float)elapsed * 1000.0) / CYCLE_NS;
+    printf("Time for 1000 divisions: %llu us\n", elapsed);
+    printf("Estimated cycles per division: %.2f\n", cycles / 1000.0);
+
     while (true) {
-        printf("Hello, world!\n");
         sleep_ms(1000);
     }
 }
